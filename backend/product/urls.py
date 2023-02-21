a9f5eb871d2dc79401533b161b98bbf2e98b2481
edolor <aghoghomena3@gmail.com>
2023-02-21 17:32:54 -0500

    Setup CustomUser Model and BaseUserManager

--- backend/product/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "product" # Namespacing this app
-
-urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
-]
--- backend/product/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views import View
-
-# Create your views here.
-class IndexView(View):
-    def get(self, request):
-        return HttpResponse("IndexPage of product")
