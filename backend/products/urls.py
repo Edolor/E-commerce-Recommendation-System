90ba1f704bb2f483922a98ace09eba6f0b0b6bb9
edolor <aghoghomena3@gmail.com>
2023-02-21 19:44:02 -0500

    Added ListView template and linked it to product detail endpoint

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = "products"
 urlpatterns = [
+    path("", views.IndexView.as_view(), name="index-view"),
     path("list/", views.product_list, name="product-list"),
-    path("detail/", views.product_detail, name="product-detail"),
+    path("detail/<uuid:pk>/", views.product_detail, name="product-detail"),
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,9 +1,8 @@
-from math import prod
-from itsdangerous import Serializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializers import ProductSerializer
 from .models import Product
+from django.views.generic import ListView
 
 @api_view(["GET"])
 def product_list(request):
@@ -12,5 +11,13 @@ def product_list(request):
     return Response(serializer.data)
 
 @api_view(["GET"])
-def product_detail(request):
-    pass
+def product_detail(request, pk):
+    product = Product.objects.get(pk=pk)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+
+class IndexView(ListView):
+    template_name = "products/index.html"
+    context_object_name = "products"
+    model = Product
