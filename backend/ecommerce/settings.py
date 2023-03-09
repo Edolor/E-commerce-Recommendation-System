0826e0070c03c043938f0009d6db29b844d15ff4
edolor <aghoghomena3@gmail.com>
2023-03-09 05:54:12 -0500

    Temp change

--- backend/ecommerce/settings.py
+++ backend/ecommerce/settings.py
@@ -143,9 +143,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# if not DEBUG:
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
