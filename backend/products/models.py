6bdf96775dfc773a9ef42d6a28d3d00cfe00144b
edolor <aghoghomena3@gmail.com>
2023-03-18 17:34:19 -0400

    Added former_price and total_quantity to model

--- backend/products/models.py
+++ backend/products/models.py
@@ -22,6 +22,8 @@ class Product(models.Model):
     category = models.ForeignKey(
         Category, related_name="products", on_delete=models.CASCADE)
     name = models.CharField(max_length=150, blank=False, null=False)
+    total_quantity = models.PositiveIntegerField(blank=False)
+    former_price = models.PositiveIntegerField(blank=True, null=True)
     description = models.TextField(max_length=250, blank=False, null=False)
     price = models.PositiveIntegerField(blank=False)
     created_at = models.DateTimeField(default=timezone.now)
