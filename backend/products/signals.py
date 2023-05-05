8cbb7451055ae134574eed1af4d559769abfc079
edolor <aghoghomena3@gmail.com>
2023-05-06 00:27:10 +0100

    Modified configurations

--- backend/products/signals.py
+++ backend/products/signals.py
@@ -2,11 +2,14 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save, post_delete
 from recommender.models import SimilarityModel
 from recommender.views import train_model_init
-from .models import Product
+from .models import Product, Image
 
 
 @receiver(post_save, sender=Product)
 def retrain_model_on_create(sender, instance, created, **kwargs):
+    """
+    Retrains recommender model when a product is added.
+    """
     if created:
         # Delete similarity model
         try:
@@ -21,6 +24,9 @@ def retrain_model_on_create(sender, instance, created, **kwargs):
 
 @receiver(post_delete, sender=Product)
 def retrain_model_on_delete(sender, instance, **kwargs):
+    """
+    Retrain recommender model when a product is deleted
+    """
     # Delete similarity model
     try:
         obj = SimilarityModel.objects.get(name="product_similarity")
@@ -30,3 +36,38 @@ def retrain_model_on_delete(sender, instance, **kwargs):
 
     # Train machine learning model
     train_model_init()
+
+@receiver(post_delete, sender=Image)
+def remove_image(sender, instance, **kwargs):
+    """
+    Delete image when product is being deleted
+    """
+    try:        
+        instance.image.delete(save=False)
+    except:
+        pass
+
+@receiver(post_save, sender=Image)
+def update_image(sender, instance, *args, **kwargs):
+    """
+    Delete old image and update it with a new image if available
+    """
+    try:
+        # Get old image
+        old_image = Image.objects.get(id=instance.id).image.path
+
+        # Get new image
+        try:
+            new_image = instance.image.path
+        except:
+            new_image = None
+            return False
+        
+        # Check if old image and new image are the same
+        if old_image != new_image:
+            import os
+            if os.path.exists(old_image): # Change to check google drive and then remove
+                os.remove(old_image)
+
+    except:
+        pass
